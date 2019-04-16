7f5138522e31bc858c4d175c34b2e853590cb653
francesco.alba <francesco.alba@flosslab.com>
2019-04-16 15:57:21 +0200

    aggiornamento versione

--- sharedmail/__openerp__.py
+++ sharedmail/__openerp__.py
@@ -22,7 +22,7 @@
 
 {
     "name": "Shared Mail",
-    "version": "8.0.1.8.6",
+    "version": "8.0.1.8.7",
     "author": "Flosslab Srl",
     "category": "Tools",
     "website": "http://www.flosslab.com",
